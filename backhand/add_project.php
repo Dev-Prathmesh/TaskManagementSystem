9486ca3e393bda18d3cb20b07f36e3cf57dc5098
prathmesh <prathameshn@remotesoftwaresolutions.com>
2022-07-05 11:35:59 +0530

    added new files

---
+++ backhand/add_project.php
@@ -0,0 +1,8 @@
+<?php
+$link = mysqli_connect("localhost", "Username", "password", "database");
+if($link===false){
+    echo"Database cannot be Connected";
+}
+else{
+   // $sql="INSERT INTO PROJECT";
+}
